45004a6dbaa1d56eabdb577f65dfa8b9ae7467e9
zhouyer <36288389+zhouyer@users.noreply.github.com>
2018-02-09 12:51:19 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package test;
+
+/**
+ * @author zhouyer
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @Title: main 
+	 * @Description: test 
+	 * @param @param args
+	 * @return void 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
